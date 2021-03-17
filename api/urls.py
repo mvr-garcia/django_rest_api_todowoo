9833486b8d8e7a9f998870d75b1a72cfb3a95b03
Marcos Garcia <mvrgarcia05@gmail.com>
2021-03-17 19:47:41 -0300

    Listing all the completed task in the API

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
-"""API URL Configuration
-
 """
-from django.contrib import admin
+API URL Configuration
+"""
 from django.urls import path
+from . import views
 
 app_name = 'api'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('todos/completed', views.TodoCompletedList.as_view()),
 ]
