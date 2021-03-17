c345d68bc9462ab8ad0a2cd8046e61a9af49818a
Marcos Garcia <mvrgarcia05@gmail.com>
2021-03-17 20:16:10 -0300

    CRUD feature added to API

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,7 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
+    path('todos', views.TodoListCreate.as_view()),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
     path('todos/completed', views.TodoCompletedList.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -11,3 +11,25 @@ class TodoCompletedList(generics.ListAPIView):
         user = self.request.user
         # Returns only user's todos completed
         return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # Returns only user's todos not completed
+        return Todo.objects.filter(user=user, datecompleted__isnull=True)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
